71b2f665fad87c5376c92ea83d541ea0253a94c9
chanaka lakshan <chanakabalasuriya123@gmail.com>
2020-12-17 15:50:06 +0530

    9.4

--- bot.js
+++ bot.js
@@ -62,23 +62,25 @@ client.on('message', msg => {
             `)
             .setColor('#FF2D00')
             .setThumbnail('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
-            .setImage('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
+            // .setImage('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
             .setFooter('This is a example footer', 'https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
             .addFields({
-                name: 'Notice',
+                name: 'Notice ⚠⚠⚠⚠📛📛📛📛',
                 value: 'We have a special steam at 7:00pm today, so if join with us ',
                 inline: true
-            }, {
-                name: '👇👇👇',
-                value: '',
-                inline: true
-            }, {
-                name: '👇👇',
-                value: ''
-            }, {
-                name: '👇',
-                value: ''
-            })
+            }
+                // , {
+                //     name: '👇👇👇',
+                //     value: '',
+                //     inline: true
+                // }, {
+                //     name: '👇👇',
+                //     value: ''
+                // }, {
+                //     name: '👇',
+                //     value: ''
+                // }
+            )
             .setTimestamp();
 
         // .addFields({

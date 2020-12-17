e9fcda639e930d53f330d170b863fd907d1f9d30
chanaka lakshan <chanakabalasuriya123@gmail.com>
2020-12-17 15:38:17 +0530

    8

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on('message', msg => {
             `)
             .setColor('#FF2D00')
             .setThumbnail('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
-            // .setImage('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
+            .setImage('')
             .setFooter('This is a example footer', 'https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
             .addFields(
                 //     {

9c52d5ae3fec0140b153ebf3cb255ecfd24e2c41
chanaka lakshan <chanakabalasuriya123@gmail.com>
2020-12-17 15:21:58 +0530

    7

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on('message', msg => {
             `)
             .setColor('#FF2D00')
             .setThumbnail('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
-            .setImage('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
+            // .setImage('https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
             .setFooter('This is a example footer', 'https://cdn1.bbcode0.com/uploads/2020/12/17/525d52e3cd2984db9171ec683c6dd598-full.png')
             .addFields(
                 //     {
@@ -72,17 +72,17 @@ client.on('message', msg => {
                 // }, 
                 {
                     name: '👇👇👇',
-                    // value: 'This is a example value',
+                    value: '',
                     inline: true
                 }, {
                 name: '👇👇',
-                // value: 'This is a example value'
+                value: ''
             }, {
                 name: '👇',
-                // value: 'This is a example value'
+                value: ''
             }, {
                 name: 'Notice',
-                value: 'We have a special steam at 7:00pm today, so if join with us ',
+                value: 'We have a special steam at 7:00pm today, so join with us 🤪',
                 inline: true
             }
             )
